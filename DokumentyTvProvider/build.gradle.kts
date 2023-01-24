36b77e0b39b0149c5cdbbd91a55d7be1fe425e91
LiJu09 <lisojuraj@gmail.com>
2023-01-24 19:53:43 +0100

    fix now

--- DokumentyTvProvider/build.gradle.kts
+++ DokumentyTvProvider/build.gradle.kts
@@ -5,8 +5,8 @@ version = 1
 cloudstream {
     // All of these properties are optional, you can safely remove them
 
-    description = "Lorem Ipsum"
-    authors = listOf("Cloudburst")
+    //description = "Lorem Ipsum"
+    //authors = listOf("Cloudburst")
 
     /**
     * Status int as the following:
@@ -20,5 +20,5 @@ cloudstream {
     // List of video source types. Users are able to filter for extensions in a given category.
     // You can find a list of avaliable types here:
     // https://recloudstream.github.io/cloudstream/html/app/com.lagradost.cloudstream3/-tv-type/index.html
-    tvTypes = listOf("Others")
+    tvTypes = listOf("Documentary")
 }
